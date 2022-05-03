cfd6e13bbd708f5a4e7da71b80c01a9cacfcfb35
brunomaria71 <brunomaria71@yahoo.com>
2022-05-03 15:28:34 -0400

    added connection

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// setup my routes 
+// setup my routes (defining which requests are allowed)
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Listening on http://localhost:3000...');
-});
+}); 
